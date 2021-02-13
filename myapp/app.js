9003840ef26cc8483a5d880b733e481f46e88850
Vipin Kumar <resetpa7@gmail.com>
2021-02-13 11:43:05 +0530

    contact gitignore added

--- myapp/app.js
+++ myapp/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var galleryRouter = require('./routes/gallery');
+var contactusRouter = require('./routes/contactus');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/gallery', galleryRouter);
+app.use('/contactus', contactusRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
