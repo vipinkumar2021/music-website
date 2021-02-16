8e1a1913c96b95bd3474e7b1ff7d5fd3de7ade67
Vipin Kumar <resetpa7@gmail.com>
2021-02-16 10:59:10 +0530

    experi

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -5,6 +5,16 @@ var adminModule = require('../modules/adminschema');
 var customerModel = require('../modules/customersignupschema');
 var employeesModel = require('../modules/employeessignupschema');
 
+// require dot env
+require('dotenv').config();
+//Crypto for creating randombytes key
+var crypto = require('crypto');
+//nodemailer for sending emails from website to clients
+var nodemailer = require('nodemailer');
+
+//encrypt passwords using bcrypt
+var bcrypt = require('bcryptjs');
+
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Music-Website', msg: '' });
