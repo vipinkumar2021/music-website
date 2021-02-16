e7036fee7f996b7f273ed58cac51073ad73f7439
Vipin Kumar <resetpa7@gmail.com>
2021-02-16 11:27:59 +0530

    change

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -25,6 +25,128 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Music-Website', msg: '' });
 });
 
+//Middleware Check username Exactly Correct One
+function checkUsername(req, res, next) {
+  var username = req.body.usrname;
+  var getCustomerData = customerModel.findOne({Username: username});
+  getCustomerData.exec((err, customerData) => {
+    if(err) throw err;
+    if(customerData) {
+
+      return res.render('index', {title: 'Front End Web Developer', msg: 'Username Already Exists'});
+    
+    }     
+    if(!customerData) {
+
+      var getEmployeeData = employeesModel.findOne({Username: username});
+      getEmployeeData.exec((err, employeeData) => {
+        if(err) throw err;
+        if(employeeData) {
+
+        return res.render('index', {title: 'Front End Web Developer', msg: 'Username Already Exists'});
+ 
+      }
+      if(!employeeData) {
+
+        var getAdminData = adminModule.findOne({Username: username});
+        getAdminData.exec((err, adminData) => {
+          if(err) throw err;
+          if(adminData) {
+
+            return res.render('index', {title: 'Front End Web Developer', msg: 'Username Already Exists'});
+ 
+          }
+          next();
+        });
+      }
+      });
+
+    }
+    //
+  });
+ }
+//Middleware Check Mobile Number Exactally Correct One
+function checkMobileNumber(req, res, next) {
+  var mobilenumber = req.body.mobilenumber;
+  var getCustomerData = customerModel.findOne({Mobilenumber: mobilenumber});
+  getCustomerData.exec((err, customerData) => {
+    if(err) throw err;
+    if(customerData) {
+
+      return res.render('index', {title: 'Front End Web Developer', msg: 'This Mobile Number is Already Registered with us'});
+    
+    }     
+    if(!customerData) {
+
+      var getEmployeeData = employeesModel.findOne({Mobilenumber: mobilenumber});
+      getEmployeeData.exec((err, employeeData) => {
+        if(err) throw err;
+        if(employeeData) {
+
+        return res.render('index', {title: 'Front End Web Developer', msg: 'This Mobile Number is Already Registered with us'});
+ 
+      }
+      if(!employeeData) {
+
+        var getAdminData = adminModule.findOne({Mobilenumber: mobilenumber});
+        getAdminData.exec((err, adminData) => {
+          if(err) throw err;
+          if(adminData) {
+
+            return res.render('index', {title: 'Front End Web Developer', msg: 'This Mobile Number is Already Registered with us'});
+ 
+          }
+          next();
+        });
+      }
+      });
+
+    }
+    
+  });
+ }
+
+ //Middleware Check Email Exactally Correct One
+ function checkEmail(req, res, next) {
+  var email = req.body.email;
+  var getCustomerData = customerModel.findOne({Email: email});
+  getCustomerData.exec((err, customerData) => {
+    if(err) throw err;
+    if(customerData) {
+
+      return res.render('index', {title: 'Front End Web Developer', msg: 'This Email is Already Registered with us'});
+    
+    }     
+    if(!customerData) {
+
+      var getEmployeeData = employeesModel.findOne({Email: email});
+      getEmployeeData.exec((err, employeeData) => {
+        if(err) throw err;
+        if(employeeData) {
+
+        return res.render('index', {title: 'Front End Web Developer', msg: 'This Email is Already Registered with us'});
+ 
+      }
+      if(!employeeData) {
+
+        var getAdminData = adminModule.findOne({Email: email});
+        getAdminData.exec((err, adminData) => {
+          if(err) throw err;
+          if(adminData) {
+
+            return res.render('index', {title: 'Front End Web Developer', msg: 'This Email is Already Registered with us'});
+ 
+          }
+          next();
+        });
+      }
+      });
+
+    }
+    
+  });
+ }
+
 
 
 module.exports = router;
