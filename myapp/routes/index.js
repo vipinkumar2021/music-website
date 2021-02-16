70540c8948f5c36b54d085ae79137c318f50f548
Vipin Kumar <resetpa7@gmail.com>
2021-02-16 11:03:19 +0530

    change done

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -16,9 +16,41 @@ var nodemailer = require('nodemailer');
 var bcrypt = require('bcryptjs');
 
 
+//jwt for creating a token
+var jwt = require('jsonwebtoken');
+// require local storage 
+if (typeof localStorage === "undefined" || localStorage === null) {
+  const LocalStorage = require('node-localstorage').LocalStorage;
+  localStorage = new LocalStorage('./scratch');
+}
+
+
+/*
 router.get('/', function(req, res, next) {
+
   res.render('index', { title: 'Music-Website', msg: '' });
 });
+*/
+
+/* GET home page. */
+router.get('/',  function(req, res, next) {
+  var loginUserCustomer = localStorage.getItem('customerLoginUserName');
+  var loginUserEmployee = localStorage.getItem('employeeLoginUserName');
+  var loginUserAdmin = localStorage.getItem('adminLoginUserName');
+  
+  if(loginUserCustomer){
+    res.redirect('/dashboardcustomer');
+  } else if(loginUserEmployee) {
+    res.redirect('/dashboardemployees');
+  } else if(loginUserAdmin) {
+    res.redirect('/dashboardadmin');
+  } else {
+    res.render('index', { title: 'SaReGaMa Music Academy & GMP Studio', msg:''});
+  }  
+});
+
+
+
 
 module.exports = router;
 
