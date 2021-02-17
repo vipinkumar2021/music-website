1cc767479c95cd575ea6399304a77d29082e9e5a
Vipin Kumar <resetpa7@gmail.com>
2021-02-17 17:55:03 +0530

    some change

--- myapp/routes/admin.js
+++ myapp/routes/admin.js
@@ -171,7 +171,7 @@ var path = require('path');
 router.use(express.static(path.join(__dirname, './public/')));
 //Set Storage Engine for file to be stored
 const storage = multer.diskStorage({
-  destination: './public/uploads/',
+  destination: 'public/uploads/', 
   filename: function(req, file, cb) {
     cb(null, file.fieldname + '_' + Date.now() + path.extname(file.originalname));
   }
