1d98128ce6d6056b92b7b1102b6fd9cf33fde46e
Vipin Kumar <resetpa7@gmail.com>
2021-02-18 11:33:24 +0530

    change

--- myapp/app.js
+++ myapp/app.js
@@ -4,6 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//Requiring bodyparser Vipin
+var bodyParser = require('body-parser');
+
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var galleryRouter = require('./routes/gallery');
